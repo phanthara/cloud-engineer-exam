80b66652e918fa3fc7c4443e3d1df6fb6c68d253
phanthara <phanthara.cha@gmail.com>
2025-04-15 16:44:29 +0700

    Initial commit: Terraform config files

---
+++ main.tf
@@ -0,0 +1,68 @@
+provider "aws" {
+  region = "ap-southeast-1" 
+}
+
+# Default VPC
+data "aws_vpc" "default" {
+  default = true
+}
+
+# Subnet
+data "aws_subnet_ids" "default" {
+  vpc_id = data.aws_vpc.default.id
+}
+
+data "aws_subnet" "default_subnet" {
+  id = data.aws_subnet_ids.default.ids[0] 
+}
+
+# Key Pair 
+resource "aws_key_pair" "deployer" {
+  key_name   = "terraform-key"
+  public_key = file("/home/phanthara/terraform-project/mykey.pub")
+}
+
+# Security Group
+resource "aws_security_group" "web_sg" {
+  name        = "web-sg"
+  description = "Allow SSH and HTTP"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] 
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# EC2 Instance
+resource "aws_instance" "web" {
+  ami                         = "ami-0ba62214afa52bec4" # Amazon Linux 2 (ap-southeast-1)
+  instance_type               = "t2.micro"
+  subnet_id                   = data.aws_subnet.default_subnet.id
+  key_name                    = aws_key_pair.deployer.key_name
+  vpc_security_group_ids      = [aws_security_group.web_sg.id]
+  associate_public_ip_address = true
+  user_data                   = file("user_data.sh")
+
+  tags = {
+    Name = "Terraform-Nginx"
+  }
+}
---
+++ user_data.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+yum update -y
+amazon-linux-extras install docker -y
+service docker start
+usermod -a -G docker ec2-user
+
+curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
+cat > /home/ec2-user/docker-compose.yml <<EOF
+version: '3'
+services:
+  web:
+    image: nginx
+    ports:
+      - "80:80"
+    volumes:
+      - ./html:/usr/share/nginx/html
+EOF
+
+mkdir -p /home/ec2-user/html
+echo "<h1>Hello from Terraform!</h1>" > /home/ec2-user/html/index.html
+
+cd /home/ec2-user
+docker-compose up -d
