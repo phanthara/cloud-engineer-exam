fa311469ade92ea5754171a6ffcdb89c066b97a9
phanthara <phanthara.cha@gmail.com>
2025-04-16 00:33:00 +0700

    Edit AMI

--- user_data.sh
+++ user_data.sh
@@ -4,7 +4,8 @@ amazon-linux-extras install docker -y
 service docker start
 usermod -a -G docker ec2-user
 
-curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" \
+  -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
 cat > /home/ec2-user/docker-compose.yml <<EOF
